9838e0b00abc3305e0055d110d194dd98d9a73d9
Florida Elago <florida.elago@gmail.com>
2012-11-08 17:10:56 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elmorfstore::Application.config.secret_token = '6c39611b89a50397b36b7e462ad57f00da2b5362aa65bbc72412d40baa1efe86745c223d552bb043373ba07615d8c5c641a821640bb1df75642e003a8ed2ba34'
---
+++ db/migrate/20121108222832_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.integer :stock_no
+
+      t.timestamps
+    end
+  end
+end
