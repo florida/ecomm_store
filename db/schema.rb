c8301a5594936997f7a3b097ec5c5aca93d86b4b
Florida Elago <florida.elago@gmail.com>
2012-11-13 10:42:48 -0600

    Added status column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108232840) do
+ActiveRecord::Schema.define(:version => 20121113164059) do
 
   create_table "lineitems", :force => true do |t|
     t.integer  "product_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20121108232840) do
     t.float    "hst"
     t.float    "gst"
     t.float    "pst"
+    t.string   "status"
   end
 
   create_table "products", :force => true do |t|
