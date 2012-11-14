8aad78202dbccd0719228d3f7b34fb6c21ea9e8e
Florida Elago <florida.elago@gmail.com>
2012-11-13 22:12:17 -0600

    added validations to url formats and email formats

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :desccription, :name, :price, :stock_no
+  attr_accessible :description, :name, :price, :stock_no, :image_url
   has_many :lineitems
   validates :description, :presence => true
   validates :name, :presence => true
   validates :price, :numericality => true, :presence => true
   validates :stock_no, :numericality => { :only_integer => true }
+  validates :image_url, :presence => true, :format => { :with => %r{\.(gif|jpe?g|png)$}i }
 end
