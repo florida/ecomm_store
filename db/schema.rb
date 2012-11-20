10fc00af64acedf5d4aa96db9a531454b330a096
Florida Elago <florida.elago@gmail.com>
2012-11-19 22:04:25 -0600

    Save the sessions in the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121113171320) do
+ActiveRecord::Schema.define(:version => 20121120040058) do
 
   create_table "lineitems", :force => true do |t|
     t.integer  "product_id"
@@ -42,7 +42,19 @@ ActiveRecord::Schema.define(:version => 20121113171320) do
     t.integer  "stock_no"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image_url"
+    t.boolean  "available"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
