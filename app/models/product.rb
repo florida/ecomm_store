5b1d02f8fa0f8dbb1f976d4ead5f841ff1cb488b
Florida Elago <florida.elago@gmail.com>
2012-11-22 11:14:29 -0600

    Installed Paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :stock_no, :image_url, :available, :category_id
+  attr_accessible :description, :name, :price, :stock_no, :image_url, :available, :category_id, :image
   scope :unavailable, where(:available => nil || :available.empty?)
   has_many :lineitems
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates :description, :presence => true
   validates :name, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0 }, :presence => true
