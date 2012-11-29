46bd6e32380ed4a1f797f812839f3a8b453eeb3f
Florida Elago <florida.elago@gmail.com>
2012-11-28 20:22:11 -0600

    Pretty forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,9 @@ module ApplicationHelper
 	def round_in_customers_favour amount
         (amount * 100).to_i / 100.0
 	end
-	
+
+	def tax_percent amount
+	  sprintf("%.2f", amount * 100) + "%"
+	end
+		
 end
